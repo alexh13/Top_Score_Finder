e59b912cdb67001b9dbdffc39ebf39123b05ae53
Alex Hughes <hughes.13.alex@gmail.com>
2019-02-26 17:38:37 -0800

    First Commit

---
+++ a5_2.cpp
@@ -0,0 +1,13 @@
+/*
+ Write a program that records high-score data for a fictitious game. The program will ask the user to enter the number
+ of scores, create two dynamic arrays sized accordingly, ask the user to enter the indicated number of names and scores,
+ and then print the names and scores sorted by score in descending order.
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Top_score_organizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Top_score_organizer.dir/main.cpp.o"
+  "Top_score_organizer.pdb"
+  "Top_score_organizer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Top_score_organizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
